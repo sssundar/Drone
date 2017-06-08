929d686ba0feb916e88101c396116b770bf0fb28
Sushant Sundaresh <ssundaresh@fitbit.com>
2017-06-07 21:39:19 -0700

    Stable PID achieved with mildly nonlinear rigid bicopter model, with ideal instantaneous motor control and perfect sensing.

--- building_controller_intuition/bicopter_pid.py
+++ building_controller_intuition/bicopter_pid.py
@@ -43,14 +43,14 @@ from time import time
 # of the bicopter with respect to the Cartesian x-axis,
 # the x-position, and the y-position, both in meters.
 #
-# Take the state to be k = (pq, q, 1/p e_q, px, x, 1/p e_x, py, y, 1/p e_y)
+# Take the state to be k = (pq, q, 1/p e_q, px, x, py, y, 1/p e_y)
 # where 
 #   p = d/dt
 #   1/p is the inverse of p (the integral from [0,t] with respect to t)
 #   e_(q,x,y) = (q,x,y) - (q_r, x_r, y_r) 
 #   *_r = set point for * 
 #
-# Consider pk = (p^2 q, pq, e_q, p^2 x, px, e_x, p^2 y, py, e_y)
+# Consider pk = (p^2 q, pq, e_q, p^2 x, px, p^2 y, py, e_y)
 # and note that if we can compute p^2 (q,x,y) from the kinematics & controllers,
 # all of pk is known, so we can numerically integrate the 1st order system.
 #
@@ -75,16 +75,15 @@ from time import time
 # Consider independent PID control of (Fdiff, Fsum) as follows, assuming the set points change 
 # as step functions, so p(q_r, x_r, y_r) ~ 0 for our purposes. Compute the following in order:
 #
-#  ie_x = 1/p e_x                                       bounded to [-awx, awx] meters as an anti-windup measure
 #  ie_y = 1/p e_y                                       bounded to [-awy, awy] meters as an anti-windup measure
-#  Fsum = Kpx * e_x + Kdx * px + Kix * ie_x +       
-#         Kpy * e_y + Kdy * py + Kiy * ie_y             bounded to [0, max_thrust]
+#  Fsum = Kpy * e_y + Kdy * py + Kiy * ie_y             bounded to [0, max_thrust]
 #  q_r = Kpx_ * e_x + Kdx_ * px                         bounded to [-max_q, max_q]
 #  ie_q = 1/p * e_q                                     bounded to [-awq, awq] as an anti-windup measure
 #  Fdiff = Kpq * e_q + Kdq * pq + Kiq * ie_q            bounded to |Fdiff| < Fsum & 0 <= F(r,l) <= max_thrust/2         
 # 
-#  We can impose the constraints as:
-#   p ie_x = e_x if (ie_x >= -awx and ie_x <= awx) or (ie_x < -awx and e_x > 0) or (ie_x > awx and e_x < 0) else 0
+# Notice we've neglected a few cross terms in these controllers, applying some physical intuition.
+# 
+# We can impose the constraints as:
 #   p ie_y = e_y if (ie_y >= -awy and ie_y <= awy) or (ie_y < -awy and e_y > 0) or (ie_y > awy and e_y < 0) else 0
 #   q_r = max(-max_q, min(Kpx_ * e_x + Kdx_ * px, max_q))
 #   p ie_q = e_q if (ie_q >= -awq and ie_q <= awq) or (ie_q < -awq and e_q > 0) or (ie_q > awq and e_q < 0) else 0
@@ -144,39 +143,40 @@ class RigidBicopter:
         self.fl_g = 0       # left thrust in gravities
         self.fr_g = 0       # right thrust in gravities
 
-        self.max_thrust  = 2*self.kg["d"]*self.g_m_per_s2
-        self.max_q       = pi/12
-        self.awq         = pi/2
-        self.awx         = 3 
-        self.awy         = 3
-        self.Kpx         = 2
-        self.Kdx         = 0.5
-        self.Kix         = 0.05
-        self.Kpy         = -2
-        self.Kdy         = -0.5
-        self.Kiy         = -0.05
-        self.Kpx_        = 2
-        self.Kdx_        = 0.5
-        self.Kpq         = -2
+        self.max_thrust  = 2*self.kg["d"]*self.g_m_per_s2       # motor limits
+        self.max_q       = pi/12                                # sanity bound
+        self.Kpy         = -5                                   # thrust controller
+        self.Kdy         = -2
+        self.Kiy         = -1
+        self.Kpx_        = 2                                    # reference pitch controller 
+        self.Kdx_        = 2
+        self.Kpq         = -2                                   # torque controller
         self.Kdq         = -0.5
         self.Kiq         = -0.05
+        self.awq         = pi/2                                 # antiwindup bounds       
+        self.awy         = 1.2*self.kg["d"]*self.g_m_per_s2/abs(self.Kiy) if abs(self.Kiy) > 0 else 0
         
         self.x_r = 0 # m
         self.y_r = 0 # m 
-        self.x_0 = -1 # m 
-        self.y_0 = -1 # m
-        self.k = asarray([0, 0, 0, 0, self.x_0, 0, 0, self.y_0, 0])
+        self.x_0 = -2 # m 
+        self.y_0 = 4 # m
+        self.k = asarray([0, 0, 0, 0, self.x_0, 0, self.y_0, 0])
     
     # This draws the line that is the bicopter
     def draw(self):        
-        (pq, q, ie_q, px, x, ie_x, py, y, ie_y) = self.k
+        (pq, q, ie_q, px, x, py, y, ie_y) = self.k
         bicopterx = asarray([x - self.r_m["~le"]*cos(q), x + self.r_m["~ri"]*cos(q)])
         bicoptery = asarray([y - self.r_m["~le"]*sin(q), y + self.r_m["~ri"]*sin(q)])
         return (bicopterx, bicoptery)
 
     # Here we have reduced the system to first order in k
     def pk(self, k_t, t):        
-        (pq, q, ie_q, px, x, ie_x, py, y, ie_y) = k_t
+        (pq, q, ie_q, px, x, py, y, ie_y) = k_t
+
+        # It's important to see whether we're close to wind up
+        # so let's save the following state for display
+        self.ie_y = ie_y
+        self.ie_q = ie_q
 
         e_x = x - self.x_r
         e_y = y - self.y_r
@@ -184,15 +184,13 @@ class RigidBicopter:
         q_r = max(-self.max_q, min(self.Kpx_ * e_x + self.Kdx_ * px, self.max_q))
         e_q = q - q_r
 
-        pie_x = e_x if ((ie_x >= -self.awx and ie_x <= self.awx) or (ie_x < -self.awx and e_x > 0) or (ie_x > self.awx and e_x < 0)) else 0
         pie_y = e_y if ((ie_y >= -self.awy and ie_y <= self.awy) or (ie_y < -self.awy and e_y > 0) or (ie_y > self.awy and e_y < 0)) else 0
         pie_q = e_q if ((ie_q >= -self.awq and ie_q <= self.awq) or (ie_q < -self.awq and e_q > 0) or (ie_q > self.awq and e_q < 0)) else 0
 
-        f_sum =  self.Kpx * e_x + self.Kdx * px + self.Kix * ie_x
-        f_sum += self.Kpy * e_y + self.Kdy * py + self.Kiy * ie_y 
+        f_sum  = self.Kpy * e_y + self.Kdy * py + self.Kiy * ie_y 
         f_diff = self.Kpq * e_q + self.Kdq * pq + self.Kiq * ie_q
 
-        f_sum = max(0, min(self.max_thrust, f_sum))
+        f_sum = max(0, min(self.max_thrust, f_sum))        
         sign_f_diff = 1 if f_diff >= 0 else -1
         f_diff = sign_f_diff * min(self.max_thrust/2, f_sum, abs(f_diff))
 
@@ -206,7 +204,7 @@ class RigidBicopter:
         p2x = -(1/self.kg["d"])*f_sum*sin(q)
         p2y = (1/self.kg["d"])*f_sum*cos(q) - self.g_m_per_s2
 
-        return [p2q, pq, pie_q, p2x, px, pie_x, p2y, py, pie_y]
+        return [p2q, pq, pie_q, p2x, px, p2y, py, pie_y]
 
     def step (self, dt):
         self.k = integrate.odeint(self.pk, self.k, [0, dt])[1]
@@ -222,34 +220,46 @@ bicopter = RigidBicopter()
 dt = 1./60      # 60 fps animation
                 # Control loop is likely updated far faster (numerically 'continuous')
                 #  as odeint is likely an adaptive step length integrator
+aim = [-2,-2]
 
 fig = plt.figure()
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-10,10), ylim=(-10,10))
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-5,5), ylim=(-5,5))
+ax.set_xlabel("x, meters")
+ax.set_ylabel("y, meters")
+ax.set_title("PID Controlled Bicopter")
 ax.grid()
 
 bicopter_body, = ax.plot([], [], 'o-', lw=2)
 time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
-left_thrust_text = ax.text(0.02, 0.90, '', transform=ax.transAxes)
-right_thrust_text = ax.text(0.02, 0.85, '', transform=ax.transAxes)
+target_text = ax.text(0.02, 0.90, '', transform=ax.transAxes)
 
 def init():
     bicopter_body.set_data([],[])
     time_text.set_text('')
-    left_thrust_text.set_text('')
-    right_thrust_text.set_text('')
-    return bicopter_body, time_text, left_thrust_text, right_thrust_text
+    target_text.set_text('')
+    return bicopter_body, time_text, target_text
 
 def animate(i):
-    global bicopter, dt
-    # This lets us repeat the simulation from the start, if we wish
-    # if i == 0:
-    #     bicopter = RigidBicopter()
+    global bicopter, dt, aim
+
+    if i % 250 == 0:
+        # When the animation is due to 'repeat,' shift our reference
+        if aim == [-2,-2]:
+            aim = [2,2]
+        elif aim == [2,2]:
+            aim = [2,-2]
+        elif aim == [2,-2]:
+            aim = [-2,2]
+        elif aim == [-2,2]:
+            aim = [-2,-2]
+        bicopter.x_r = aim[0]
+        bicopter.y_r = aim[1]
+
     bicopter.step(dt)    
     bicopter_body.set_data(*bicopter.draw())        
-    time_text.set_text('time = %0.1f s' % bicopter.t_s)
-    left_thrust_text.set_text('Fl = %0.5f g' % bicopter.fl_g) 
-    right_thrust_text.set_text('Fr = %0.5f g' % bicopter.fr_g)
-    return bicopter_body, time_text, left_thrust_text, right_thrust_text
+    time_text.set_text('time = %0.1f s' % bicopter.t_s)             # time in seconds
+    target_text.set_text('(x, y) => (%d, %d) m' % (aim[0], aim[1]))  # reference (target position)
+    return bicopter_body, time_text, target_text
 
 # This delays between frames so we hit our fps target
 t0 = time()
@@ -257,7 +267,7 @@ animate(0)
 t1 = time()
 interval = 1000 * dt - (t1 - t0)
 
-ani = animation.FuncAnimation(fig, animate, frames=200, repeat=True, interval=interval, blit=True, init_func=init)
-#ani.save('bicopter.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
+ani = animation.FuncAnimation(fig, animate, frames=1250, repeat=False, interval=interval, blit=True, init_func=init)
+ani.save('bicopter.mp4', fps=60, extra_args=['-vcodec', 'libx264'])
 
-plt.show()
+# plt.show()
