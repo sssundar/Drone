e48b73c5fa29d1f59a30fcc8a232efd7b1bd8a17
Sushant Sundaresh <ssundaresh@fitbit.com>
2018-01-11 10:47:03 -0800

    Simple integration shows sloooow divergence of angular velocity magnitude with zero torque with adaptive integration. Is fine.

--- rotation/demo.py
+++ rotation/demo.py
@@ -22,14 +22,18 @@ from numpy import cross as cross
 from numpy import dot as dot
 from numpy import transpose as transpose
 from numpy.linalg import inv as inverse
+from numpy import linspace as linspace
+from numpy import sqrt
+
+from scipy.integrate import odeint
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-global Jb
-global Jb_inv
-global wb_0
-global O_0
+Jb = None
+Jb_inv = None
+wb_0 = None
+O_0 = None
 AX_ROLL, AX_PITCH, AX_YAW = (0, 1, 2)
 
 def setup():
@@ -40,7 +44,7 @@ def setup():
   Jb[1,1] = 3.25
   Jb[2,2] = 6.25
 
-  Jb_inv = inverse(J_b)
+  Jb_inv = inverse(Jb)
 
   O_0 = np.zeros(3)
 
@@ -50,8 +54,7 @@ def setup():
   wb_0[AX_YAW] = pi/4
 
 def ddt_wb(wb, t):
-  global Jb
-  global Jb_inv
+  global Jb, Jb_inv
   return dot(Jb_inv, cross(dot(Jb, wb), wb))
 
 def HIB(roll, pitch, yaw):
@@ -109,12 +112,42 @@ def Lbi(roll, pitch, yaw):
   return L
 
 def Main():
+  global wb_0, O_0
+
   setup()
 
   # Sampling period of body angular velocity
-  T_wb = 0.02 # s
+  T_wb_s = 0.02
 
-  # Integrate d/dt wb = ddt_wb(wb, t). Sample it at T_wb
+  # Integrate d/dt wb = ddt_wb(wb, t). Sample it at T_wb.
+  T_final_s = 100.0
+  N_points = int(T_final_s / T_wb_s)
+  time_s = linspace(0, T_final_s, N_points)
+  wb = odeint(ddt_wb, wb_0, t = time_s)
+
+  # Does the length of angular velocity without external torque increase with
+  # time? Well, yes, but to the tune of 8E-7 per 100s. So... orders of magnitude
+  # less than any noise. I'd ignore it.
+  # wb_mag = [sqrt(dot(transpose(wb[k]),wb[k])) for k in xrange(len(wb))]
+  # plt.plot(time_s, wb_mag)
+  # plt.show()
+
+  # Can you just plot wb over time in 3d space?
+  N_decimation = 20
+  X = [0 for k in wb[::N_decimation]]
+  Y = [0 for k in wb[::N_decimation]]
+  Z = [0 for k in wb[::N_decimation]]
+  U = [k[0] for k in wb[::N_decimation]]
+  V = [k[1] for k in wb[::N_decimation]]
+  W = [k[2] for k in wb[::N_decimation]]
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.quiver(X,Y,Z,U,V,W, length=0.1, arrow_length_ratio=0.05, pivot='tail')
+  ax.set_xlabel("x")
+  ax.set_ylabel("y")
+  ax.set_zlabel("z")
+  plt.show()
 
   # For each w_b(t), compute dO/dt and integrate it over time-steps of T_o << T_wb. Assume wb is constant over the T_wb window.
 
