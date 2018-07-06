e1a136a063844834128391b8dd1163a429b7bb4e
Zeo Liu <zeo.liu@fitbit.com>
2018-07-05 21:40:11 -0700

    how about now

--- rotation/kalman_filter/state_estimation.py
+++ rotation/kalman_filter/state_estimation.py
@@ -104,8 +104,8 @@ def naive(hpf):
 
 
     new_part = mu * grad_f / vector_norm(grad_f)
-    hohoho = np.array([new_part[1], new_part[2], new_part[3]])
-    q = [r_i[-1][0] - new_part[0], r_i[-1][1] - hohoho] 
+    hehehe = np.array([new_part[1], new_part[2], new_part[3]])
+    q = [r_i[-1][0] - new_part[0], r_i[-1][1] - hehehe] 
 
     r_i.append(q)
 
