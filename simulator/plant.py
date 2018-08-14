86aac2db52d558f9ec33ade816f22b819aa549b3
Sushant Sundaresh <ssundaresh@fitbit.com>
2018-08-14 16:31:48 -0700

    Plant Interface and Path Forward.

--- simulator/plant.py
+++ simulator/plant.py
@@ -2,53 +2,51 @@ import sys
 import numpy as np
 from scipy.integrate import odeint
 from quaternions import *
-from matplotlib import pyplot as plt
-from animate import generate_body_frames, animate
+#
+# @brief      Initializes a horizontal quad oriented with the Earth's magnetic field, motors halted.
+#
+def plant_init():
+  pass
 
-# @brief Simulates gyrometer and 3D compass readings during a rigid free-body rotation subject to zero external torque in a time-invariant local magnetic field.
 #
-# @param[in] inputs, a dictionary with the following fields:
-#             - r_i_bp: [axis (unit numpy 3-vector), angle (radians)]
-#                 an initial condition. how to rotate the standard inertial frame to the principal body frame
-#                 specified from the standard inertial frame!
-#             - r_bp_b: [axis (unit numpy 3-vector), angle (radians)]
-#                 an initial condition. how to rotate the principal body frame to the actual body frame.
-#                 specified from the principal body frame!
-#             - J_bp: a numpy 3x3 matrix with x,y,z as indices 0,1,2, respectively. units kg m^2
-#                 the diagonal 3x3 moment of inertia matrix seen from the principal body frame
-#             - w_bp: a numpy 3-vector [rad/s]
-#                 an initial condition. the angular velocity seen from the principal body frame
-#             - f_s: a scalar. units Hz
-#                 the sampling frequency for simulation
-#             - t_f: a scalar. units seconds.
-#                 the final simulation time, from zero.
-#             - m_i: a numpy 3-vector. unitless.
-#                 a unit vector representing the direction of the Earth's local magnetic field when the body frame is coincident with the standard inertial frame.
-#                 note we could input a time-average while the device is booting (aka require horizontal calibration)
-#                                     a hard-coded lookup of the local declination...
-#                 note even if we're wrong in this... our controller will just adjust the reference attitude until we come to rest at our desired position.
-#                 it will think it is tilted, but really the mechanics will require it to be horizontal to be at rest.
-#             - a_i: a numpy 3-vector. unitless.
-#                 a unit vector representing the direction of the Earth's local gravitational field when the body frame is coincident with the standard inertial frame.
+# @brief      Simulates the plant dynamics of a quadcopter without external
+#             disturbances.
+#               - Reproduces the measured linear duty-cycle to thrust relation
+#                 for DC brushed coreless motors
+#               - Accounts for gyroscopic effects from the propellors
+#               - Accounts for IMU-frame vs. body-frame mismatch
+#               - Accounts for motor drive asymmetry
+#               - Assumes a rectangular symmetric chassis
 #
-# @return outputs, a dictionary with the following fields:
-#             - m_i: a numpy 3-vector. unitless.
-#                 a unit vector representing the direction of the Earth's local magnetic field when the body frame is coincident with the standard inertial frame.
-#                 intended as an input to the realism module
-#             - a_i: a numpy 3-vector. unitless.
-#                 a unit vector representing the direction of the Earth's local gravitational field when the body frame is coincident with the standard inertial frame.
-#                 intended as an input to the realism module
-#             - t_s: a list of sample times (seconds) of length (t_f - 0) * f_s
-#             - q_b: a list of quaternions [r, v] where v is a numpy 3-vector
-#                    of the same length as t
-#                 represents the 'ground truth' coordinate transformations. rotating a vector v_b by q_b yields v_i. computed without sampling noise, bias, etc.
-#             - w_b: a list of numpy 3-vectors [rad/s] representing ideal samples from a gyroscope coincident with the body frame
-#                 intended as an input to the realism module
-#             - m_b: a list of numpy 3-vectors [unit norm, unitless] representing ideal samples of the direction of the Earth's magnetic field from a 3D compass coincident with the body frame
-#                 intended as an input to the realism module
-#             - a_b: a list of numpy 3-vectors [unit norm, unitless] representing ideal samples of the direction of the Earth's gravitational field from a 3D accelerometer coincident with the body frame
-#                 intended as an input to the realism module
-def simulate(inputs):
+# @param[in]  duty  - a dictionary
+#                   - keys {m1p2, p1p2, p1m2, m1m2} representing (m)inus and
+#                     (p)lus body axes 1,2
+#                   - values representing motor-drive PWM duty-cycles between
+#                     [0,1]
+# @param[in]  dt    the time-step to simulate, in seconds
+#
+# @return     outputs, a dictionary with the following fields, computed without
+#             noise, bias, jitter, or delay:
+#             - w: a numpy 3-vector [rad/s] representing a sample from a 3D
+#               gyroscope on the quad
+#             - m: a numpy 3-vector [unit norm, unitless] representing a sample
+#               of the direction of the Earth's magnetic field from a 3D compass
+#               on the quad
+#             - a: a numpy 3-vector [unit norm, unitless] representing a sample
+#               of the direction of the Earth's gravitational field from a 3D
+#               accelerometer on the quad
+#             - q: a quaternion [r, v], where v is a numpy 3-vector,
+#               representing the coordinate transformation from the quad
+#               body-frame to the space frame. rotating a vector from the quad
+#               body frame by q_b yields a space-frame representation. note that
+#               the IMU-frame is not necessarily concident with the quad body
+#               frame.
+#             - r: a numpy 3-vector representing the center of mass of the quad
+#               in the space frame.
+#
+def plant_evolve(duty, dt):
+  # Simulate in the quad frame, then rotate samples to the IMU frame.
+
   # Get the time step for this simulation
   dt = 1.0 / inputs["f_s"] # seconds
 
@@ -101,89 +99,3 @@ def simulate(inputs):
 
   return outputs
 
-def check_principal(axis):
-  # For instance, check a single axis 1 Hz rotation where the principal body axis IS the body axis,
-  # and make sure time, orientations, ... are correct. This will also help you flesh out the animation code.
-  inputs = {}
-  inputs["r_i_bp"] = [np.asarray([0,0,1]), 0] # Standard inertial frame IS the principal body frame (initially)
-  inputs["r_bp_b"] = [np.asarray([0,0,1]), 0] # Principal body frame IS the actual body frame
-  inputs["J_bp"] = (1.0/6) * 0.05 * np.eye(3) # A uniform cubic 50g mass of length 1 m has J = M/12 I where M is the total mass.
-  if axis == 'x':
-    inputs["w_bp"] = 2*np.pi*np.asarray([1,0,0]) # 1 Hz CCW rotation about the body x-axis is a 1 Hz CCW rotation about the inertial x axis.
-  elif axis == 'y':
-    inputs["w_bp"] = 2*np.pi*np.asarray([0,1,0]) # 1 Hz CCW rotation about the body y-axis is a 1 Hz CCW rotation about the inertial y axis.
-  else:
-    inputs["w_bp"] = 2*np.pi*np.asarray([0,0,1]) # 1 Hz CCW rotation about the body z-axis is a 1 Hz CCW rotation about the inertial z axis.
-  inputs["f_s"] = 100.0 # Hz
-  inputs["t_f"] = 1.0 # seconds
-  inputs["m_i"] = np.asarray([1.0/2, 0, np.sqrt(3)/2])  # Normalized magnetic field points in x,z directions when the body is aligned with the inertial frame.
-  inputs["a_i"] = np.asarray([0,     0, 1])             # Normalized gravitational field points straight up when the body is aligned with the inertial frame.
-
-  outputs = simulate(inputs)
-
-  # Check: Animate it. Orientations look right? Rotating about chosen inertial axis CCW (+)?
-  # Status: Pass.
-  e0, e1, e2 = generate_body_frames(outputs["q_b"])
-  N_frames = len(outputs["t_s"])
-  animate(N_frames, e0, e1, e2, 5)
-
-def check_misalignment():
-  # Do the initial rotations of the principal and body axes relative to the inertial frame look right?
-  inputs = {}
-  inputs["r_i_bp"] = [np.asarray([1,0,0]), np.pi/2] # Principal body frame is initially rotated 90 degrees about the inertial x axis.
-  inputs["r_bp_b"] = [np.asarray([0,1,0]), np.pi/4] # Actual body frame is rotated 45 degrees about the principal y axis
-  inputs["J_bp"] = (1.0/6) * 0.05 * np.eye(3) # A uniform cubic 50g mass of length 1 m has J = M/6 I where M is the total mass.
-  inputs["w_bp"] = 2*np.pi*np.asarray([0,0,1]) # 1 Hz CCW rotation about the principal body z-axis, initially.
-  inputs["f_s"] = 100.0 # Hz
-  inputs["t_f"] = 1.0 # seconds
-  inputs["m_i"] = np.asarray([1.0/2, 0, np.sqrt(3)/2])  # Normalized magnetic field points in x,z directions when the body is aligned with the inertial frame.
-  inputs["a_i"] = np.asarray([0,     0, 1])             # Normalized gravitational field points straight up when the body is aligned with the inertial frame.
-
-  outputs = simulate(inputs)
-
-  # Check: Animate it. Initial orientation looks right?
-  # Status: Pass.
-  e0, e1, e2 = generate_body_frames(outputs["q_b"])
-  N_frames = len(outputs["t_s"])
-  animate(N_frames, e0, e1, e2, 100)
-
-def check_precession():
-  # Let's misalign our body and principal body axes. Do we precess?
-  inputs = {}
-  inputs["r_i_bp"] = [np.asarray([1,0,0]), np.pi/2] # Principal body frame is initially rotated 90 degrees about the inertial x axis.
-  inputs["r_bp_b"] = [np.asarray([0,1,0]), np.pi/10] # Actual body frame is rotated 18 degrees about the principal y axis
-  inputs["J_bp"] = np.zeros([3,3])
-  inputs["J_bp"][0,0] = 1
-  inputs["J_bp"][1,1] = 2
-  inputs["J_bp"][2,2] = 3
-  inputs["w_bp"] = 2*np.pi*np.asarray([0,0,1]) # 1 Hz CCW rotation about the principal body z-axis, initially.
-  inputs["f_s"] = 800.0 # Hz
-  inputs["t_f"] = 1.0 # seconds
-  inputs["m_i"] = np.asarray([1.0/2, 0, np.sqrt(3)/2])  # Normalized magnetic field points in x,z directions when the body is aligned with the inertial frame.
-  inputs["a_i"] = np.asarray([0,     0, 1])             # Normalized gravitational field points straight up when the body is aligned with the inertial frame.
-
-  outputs = simulate(inputs)
-
-  # Check: Animate it. Does it look reasonable?
-  # Status: Pass.
-  e0, e1, e2 = generate_body_frames(outputs["q_b"])
-  N_frames = len(outputs["t_s"])
-  animate(N_frames, e0, e1, e2, 4)
-
-if __name__ == "__main__":
-  # Run some sanity checks!
-  if len(sys.argv) == 3:
-    if sys.argv[1] == 'check_principal':
-      check_principal(sys.argv[2])
-  elif len(sys.argv) == 2:
-    if sys.argv[1] == 'check_misalignment':
-      check_misalignment()
-    elif sys.argv[1] == 'check_precession':
-      check_precession()
-  else:
-    print ""
-    print "Usage: python free_body.py check_principal x|y|z"
-    print "Usage: python free_body.py check_misalignment"
-    print "Usage: python free_body.py check_precession"
-    print ""
-    sys.exit(0)
