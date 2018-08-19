93daf270f4e566c26f65b5189dbdc864a1fc8b53
Sushant Sundaresh <ssundaresh@fitbit.com>
2018-08-18 23:01:13 -0700

    Free Fall simulation reveals that turning on one of the motors impacts the gyro but not the center of mass motion. That's a bug

--- simulator/controller.py
+++ simulator/controller.py
@@ -6,19 +6,43 @@ def FreeFall():
   dt=0.01
   t_s = np.asarray(range(100))*dt
   quad = Plant(dt=dt)
-  duty_cycles = {
+  u = {
     "m1p2m3" : 0.0,
     "p1p2p3" : 0.0,
     "p1m2m3" : 0.0,
     "m1m2p3" : 0.0
     }
+  gyro = []
+  compass = []
+  accel = []
   orientations = []
   cm = []
   for k in xrange(100):
-    (_, _, _, orientation, center_of_mass) = quad.evolve(duty_cycles)
-    orientations.append(orientation)
-    cm.append(center_of_mass)
+    (w, m, a, q, r) = quad.evolve(u)
+    gyro.append(w)
+    compass.append(m)
+    accel.append(a)
+    orientations.append(q)
+    cm.append(r)
+
+  plt.subplot(211)
   plt.plot(t_s, cm)
+  plt.legend(["x", "y", "z"])
+  plt.ylabel("m")
+  plt.title("An xy-stationary quad in free fall drops 4.9 meters in one second.\n")
+
+  # Assume the IMU-to-quad frame offset is known perfectly, through offline calibration.
+  offset = lambda v: [quaternion_rotation(qv=[0,x], qr=quaternion_inverse(quad.config["q_offset"]))[1] for x in v]
+
+  plt.subplot(212)
+  plt.plot(t_s, offset(gyro))
+  plt.plot(t_s, offset(compass))
+  plt.plot(t_s, offset(accel))
+  plt.legend(["g_x", "g_y", "g_z", "m_x", "m_y", "m_z", "a_x", "a_y", "a_z"])
+  plt.ylabel("rad/s, unitless, m/s^2")
+  plt.xlabel("s")
+  plt.title("An IMU cannot observe free-fall.\n")
+
   plt.show()
 
 if __name__ == "__main__":
