Bug in either plant IMU-mock or estimator. We aren't tracking even a remotely a 3D rotation. 1D is fine.

simulator/controller.py
@@ -3,10 +3,10 @@ import numpy as np
 class Controller(object):
   def __init__(self):
     self.duty_cycles = {
-      "m1p2m3" : 0.50,
-      "p1p2p3" : 0.25,
-      "p1m2m3" : 0.25,
-      "m1m2p3" : 0.25
+      "m1p2m3" : 0.49+0.01+0.01,
+      "p1p2p3" : 0.49+0.01,
+      "p1m2m3" : 0.49+0.01,
+      "m1m2p3" : 0.49
     }
     self.reference = {
       "r" : np.asarray([0, 0, 0]),

simulator/estimation.py
@@ -89,7 +89,7 @@ class Estimator(object):
     # Note beta can compensate for bias if it is large enough.
     #   Then... you'll jitter if there's mag/acc noise. You don't want to make beta unnecessarily large.
     self.beta = np.pi/2   # ~18 dps spread iid in all axes. If this is zero we're just integrating without any orientation compensation
-    self.zeta = np.pi/3    # ~60 dps bias error iid in all axes. If this is zero we don't use gyro bias compensation.
+    self.zeta = np.pi/3   # ~60 dps bias error iid in all axes. If this is zero we don't use gyro bias compensation.
 
     # Bias Accumulator
     self.int_we = np.asarray([0.0,0.0,0.0])