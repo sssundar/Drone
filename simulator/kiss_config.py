e46b19d84c57684bd9ce797f6556516c71db6546
Sushant Sundaresh <sushant.sundaresh@gmail.com>
2019-12-12 22:09:43 -0800

    Linearizing about equilibrium point and scanning for PID coefficients shows I've been using coefficients far too small to stabilize the y-jig. At least... so says the simulation. Let's see what happens in real life when I port this 1D controller.

---
+++ simulator/kiss_config.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+dt_s = 0.05
+n = 1000
+
+alpha = 1.
+theta_ref = 0.
+theta_initial = +np.pi/6
+
+beta = 3.87e+02
+J = 0.834
+gamma_f = 4.
+gamma_g = beta*0.1
+phi = 0.08
+tau = 1./3
+asymmetry = 0.05
+beta1 = beta * (1-asymmetry)
+beta2 = beta * (1+asymmetry)
+
+CLIP = {"integrator": beta/6, "total": beta/4}
--- simulator/kiss_pid.py
+++ simulator/kiss_pid.py
@@ -1,66 +1,30 @@
 # This is a K.I.S.S. prototyping script for PID control of 
-# a 1D rotation in the y-jig with perfect observation.
+# a 1D rotation in the y-jig with perfect observation,
+# gravity torque, and imperfectly known motor asymmetry.
 
 import sys
 import numpy as np
 from scipy.integrate import odeint
-from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from kiss_config import *
 
-dt_s = 0.05
-n = 100
-
-theta_ref = 0.
-
-kp = 10.
-kd = 2.
-ki = 0.1
-integrator_clip = 0.5
-torque_clip = 1.2
-
-beta = 3.87732955e+02
-Jy = 0.834
-T_friction = 4
-T_g = 387*0.1
-eq_phase = 0.08
-T_pole = 3
-
-asymmetry = 0.05
-T_max = [beta*(1-asymmetry), beta*(1+asymmetry), beta*(1+asymmetry), beta*(1-asymmetry)],
-
-theta_0 = +np.pi/6
-
-cfg = [  [1, T_max[0], -1., 'r-'],
-      [2, T_max[1], 1., 'g-'],
-      [3, T_max[2], 1., 'b-'],
-      [4, T_max[3], -1., 'k-'], ]
-
-get_motorid = lambda annot: annot[0]
-color = lambda mid: cfg[mid-1][-1]
-motor_sign = lambda mid: cfg[mid-1][-2]
-motor_max_torque = lambda mid: cfg[mid-1][-3]
-
-def simulate():
+def simulate(kp, kd, ki):
   def ddt_state(state, t):
-    theta, w, m1, m2, m3, m4, d1, d2, d3, d4 = state
-    ddt = np.zeros(10)
+    theta, w, m1, m2, d1, d2 = state
+    ddt = np.zeros(6)
     ddt[0] = w
-    net_m = motor_sign(1)*m1 + motor_sign(2)*m2 + motor_sign(3)*m3 + motor_sign(4)*m4
-    ddt[1] = (1./Jy)*(net_m + T_g*np.sin(theta+eq_phase) - np.sign(w)*np.abs(w)*T_friction)
-
-    ddt[2] = -T_pole*m1 + d1*motor_max_torque(1)*T_pole
-    ddt[3] = -T_pole*m2 + d2*motor_max_torque(2)*T_pole
-    ddt[4] = -T_pole*m3 + d3*motor_max_torque(3)*T_pole
-    ddt[5] = -T_pole*m4 + d4*motor_max_torque(4)*T_pole
-
+    ddt[1] = (1./J)*(-m1 + m2 + gamma_g*np.sin(theta+phi) - gamma_f*w)
+    ddt[2] = (1./tau) * (d1*beta1 - m1)
+    ddt[3] = (1./tau) * (d2*beta2 - m2)
     return ddt
 
-  thetas = [theta_0]
+  thetas = [theta_initial]
   ws = [0.]
-  ms = [[0.]*4]
+  ms = [[0.]*2]
   us = [0.]
-  ds = [np.zeros(4)]
-
-  integrator = 0.
+  ds = [np.zeros(2)]
+  zetas =[0.]
 
   for idx in range(n):
     percent = (100.0*idx)/n
@@ -76,51 +40,114 @@ def simulate():
     # Plant Dynamics
     state_0 = np.array([
                   theta_0, w_0,
-                  m_0[0], m_0[1], m_0[2], m_0[3],
-                  d_0[0], d_0[1], d_0[2], d_0[3],
+                  m_0[0], m_0[1],
+                  d_0[0], d_0[1],
                 ])
-
     state_dt = odeint(ddt_state, state_0, t=[0, dt_s])[1]
 
     # Perfect Observation
     theta_dt = state_dt[0]
     w_dt = state_dt[1]
-    m_dt = [state_dt[2], state_dt[3], state_dt[4], state_dt[5]]
-
+    m_dt = [state_dt[2], state_dt[3]]
     thetas.append(theta_dt)
     ws.append(w_dt)
     ms.append(m_dt)
 
-    # Controller Update with theta_ref = 0
+    # Controller Update
     theta_err = theta_ref - theta_dt
     proportional = kp*theta_err
     derivative = -kd*w_dt
-    
-    integrator += dt_s * theta_err
-    integrator = np.maximum(np.minimum(integrator, integrator_clip), -integrator_clip)
-    integral = ki*integrator
-
+    zeta = zetas[-1] + (theta_err * dt_s)
+    zeta = np.maximum(np.minimum(zeta, CLIP["integrator"]), -CLIP["integrator"])
+    zetas.append(zeta)
+    integral = ki*zeta
     u_dt = proportional + derivative + integral
-    u_dt = np.maximum(np.minimum(u_dt, torque_clip), -torque_clip)
+    u_dt = np.maximum(np.minimum(u_dt, CLIP["total"]), -CLIP["total"])
     us.append(u_dt)
-
-    TODO
-    d_dt = np.dot(Dp_inv, np.array(list(u_dt)+[0.8]))
-
-    for k in range(4):
+    d_dt = 0.5*np.array([alpha - (u_dt/beta), alpha + (u_dt/beta)])
+    for k in range(2):
       if d_dt[k] > 1.0:
         d_dt[k] = 1.0
       elif d_dt[k] < 0.0:
         d_dt[k] = 0.0
-
     ds.append(d_dt)
 
-  fig, ax = plt.subplots(2,1,sharex=True)
-  ax[0].plot(np.asarray(range(n+1))*dt_s, ds)
-  ax[0].legend(["d1", "d2", "d3", "d4"])
-  ax[1].plot(np.asarray(range(n+1))*dt_s, us)
-  ax[1].legend(["y"])
+  t_s = np.asarray(range(n+1))*dt_s
+  fig, ax = plt.subplots(6,1,sharex=True)
+  ax[0].plot(t_s, us)
+  ax[0].legend(["u"])
+  ax[1].plot(t_s, zetas)
+  ax[1].legend("zeta")
+  ax[2].plot(t_s, ds)
+  ax[2].legend(["d1", "d2"])
+  ax[3].plot(t_s, [x[0] for x in ms], 'r-')
+  ax[3].plot(t_s, [x[1] for x in ms], 'b-')
+  ax[3].legend(["m1", "m2"])
+  ax[4].plot(t_s, ws)
+  ax[4].legend("w")
+  ax[5].plot(t_s, thetas, "k-")
+  ax[5].plot(t_s, np.ones(len(t_s))*theta_ref, 'r--')
+  ax[5].legend(["theta", "ref"])
   plt.show()
 
+# Eigenvalues of Near-Equilibrium Linearized Dynamics
+def dynamics(kp, kd, ki):
+  return np.linalg.eigvals(np.array([
+    [0, 1, 0, 0, 0],
+    [(1./J)*gamma_g*np.cos(theta_ref - phi), -gamma_f/J, -1./J, 1./J, 0],
+    [(beta1/(2*tau*beta))*kp, (beta1/(2*tau*beta))*kd, -1./tau, 0, -(beta1/(2*tau*beta))*ki],
+    [-(beta2/(2*tau*beta))*kp, -(beta2/(2*tau*beta))*kd, 0, -1./tau, (beta2/(2*tau*beta))*ki],
+    [-1, 0, 0, 0, 0],
+    ]))
+
+def scan():
+  kps = np.logspace(-2, 2, 20)
+  kds = np.logspace(-2, 2, 20)
+  kis = np.logspace(-2, 2, 20)
+  x = []
+  y = []
+  z = []
+  color = []
+  for kp in kps:
+    for kd in kds:
+      for ki in kis:
+        x.append(kp)
+        y.append(kd)
+        z.append(ki)
+        color.append('k' if np.all(np.real(dynamics(kp, kd, ki)) <= 0) else 'r')
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+  ax.scatter(x, y, z, c=color, marker='o')
+  ax.set_xlabel('kp')
+  ax.set_ylabel('kd')
+  ax.set_zlabel('ki')
+  plt.show()
+
+def equilibrium(ki):
+  # Equilibrium
+  M_inv = np.linalg.inv(np.array([
+    [1, -1, 0],
+    [1, 0, beta1*ki/(2*beta)],
+    [0, 1, -beta2*ki/(2*beta)],
+    ]))
+  b = np.array([gamma_g*np.sin(theta_ref+phi), beta1*alpha/2, beta2*alpha/2])
+  x_eq = np.array([0, 0] + list(np.dot(M_inv, b)))
+  print("theta_eq: %0.2f\nw_eq: %0.2f\nm1_eq: %0.2f\nm2_eq: %0.2f\nzeta_eq: %0.2f\n" % tuple(x_eq))
+
 if __name__ == "__main__":
-  simulate()
+  if (len(sys.argv) != 2):
+    print("Usage: (laptop) python kiss_pid.py [scan|eq|sim]")
+    sys.exit(1)
+
+  if sys.argv[1] == "scan":
+    scan()
+  elif sys.argv[1] == "eq":
+    equilibrium(ki=10)
+  elif sys.argv[1] == "sim":
+    kp = 60.
+    kd = 40.
+    ki = 20.
+    equilibrium(ki)
+    print(dynamics(kp, kd, ki))
+    simulate(kp, kd, ki)
