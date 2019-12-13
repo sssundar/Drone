f16f1ad2d6d29277246ef89ff72a395c52adbde4
Sushant Sundaresh <sushant.sundaresh@gmail.com>
2019-12-12 18:04:24 -0800

    Fleshing out 1D controller.

--- simulator/kiss_pid.py
+++ simulator/kiss_pid.py
@@ -6,6 +6,17 @@ import numpy as np
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 
+dt_s = 0.05
+n = 100
+
+theta_ref = 0.
+
+kp = 10.
+kd = 2.
+ki = 0.1
+integrator_clip = 0.5
+torque_clip = 1.2
+
 beta = 3.87732955e+02
 Jy = 0.834
 T_friction = 4
@@ -14,11 +25,9 @@ eq_phase = 0.08
 T_pole = 3
 
 asymmetry = 0.05
-T_max = [-beta*(1-asymmetry), beta*(1+asymmetry), beta*(1+asymmetry), -beta*(1-asymmetry)],
+T_max = [beta*(1-asymmetry), beta*(1+asymmetry), beta*(1+asymmetry), beta*(1-asymmetry)],
 
 theta_0 = +np.pi/6
-w_0 = 0
-T_0 = 0
 
 cfg = [  [1, T_max[0], -1., 'r-'],
       [2, T_max[1], 1., 'g-'],
@@ -36,14 +45,22 @@ def simulate():
     ddt = np.zeros(10)
     ddt[0] = w
     net_m = motor_sign(1)*m1 + motor_sign(2)*m2 + motor_sign(3)*m3 + motor_sign(4)*m4
-    ddt[1] = (1./Jy)*(net_m + T_g*s(theta+eq_phase) - np.sign(w)*np.abs(w)*T_friction)
-    TODO
-    ddt[2] = -T_pole*motor_torque + dc*T_max*T_pole
+    ddt[1] = (1./Jy)*(net_m + T_g*np.sin(theta+eq_phase) - np.sign(w)*np.abs(w)*T_friction)
+
+    ddt[2] = -T_pole*m1 + d1*motor_max_torque(1)*T_pole
+    ddt[3] = -T_pole*m2 + d2*motor_max_torque(2)*T_pole
+    ddt[4] = -T_pole*m3 + d3*motor_max_torque(3)*T_pole
+    ddt[5] = -T_pole*m4 + d4*motor_max_torque(4)*T_pole
+
     return ddt
 
-  t_sim = np.linspace(0, t_final_s, 1000)
-  state_0 = np.array([theta_0, w_0, T_0])
-  state = odeint(ddt_state, state_0, t=t_sim)
+  thetas = [theta_0]
+  ws = [0.]
+  ms = [[0.]*4]
+  us = [0.]
+  ds = [np.zeros(4)]
+
+  integrator = 0.
 
   for idx in range(n):
     percent = (100.0*idx)/n
@@ -51,51 +68,45 @@ def simulate():
     sys.stdout.write('\rSimulation ' + complete + " complete.")
     sys.stdout.flush()
 
-    q_0 = qs[-1]
+    theta_0 = thetas[-1]
     w_0 = ws[-1]
+    m_0 = ms[-1]
     d_0 = ds[-1]
 
     # Plant Dynamics
     state_0 = np.array([
-                  w_0[0], w_0[1], w_0[2],
-                  q_0[0], q_0[1][0], q_0[1][1], q_0[1][2],
-                  d_0[0], d_0[1], d_0[2], d_0[3]
+                  theta_0, w_0,
+                  m_0[0], m_0[1], m_0[2], m_0[3],
+                  d_0[0], d_0[1], d_0[2], d_0[3],
                 ])
 
     state_dt = odeint(ddt_state, state_0, t=[0, dt_s])[1]
 
     # Perfect Observation
-    w_dt = np.array([state_dt[0], state_dt[1], state_dt[2]])
-    q_dt = [state_dt[3], np.array([state_dt[4], state_dt[5], state_dt[6]])]
-    q_dt = quaternion_product(p=[1, np.asarray([0,0,0])], q=q_dt, normalize=True)
+    theta_dt = state_dt[0]
+    w_dt = state_dt[1]
+    m_dt = [state_dt[2], state_dt[3], state_dt[4], state_dt[5]]
 
-    qs.append(q_dt)
+    thetas.append(theta_dt)
     ws.append(w_dt)
+    ms.append(m_dt)
 
-    # Controller Update
-    q_error = quaternion_product(p=quaternion_inverse(q_dt), q=q_ref, normalize=True)
-    theta = np.arccos(q_error[0])
-    sin_theta = np.sin(theta)
-    if abs(sin_theta) < 1e-4:
-      w_error = np.array([0.0, 0.0, 0.0])
-      theta = 0.0
-    else:
-      w_error = q_error[1] / np.sin(theta)
+    # Controller Update with theta_ref = 0
+    theta_err = theta_ref - theta_dt
+    proportional = kp*theta_err
+    derivative = -kd*w_dt
+    
+    integrator += dt_s * theta_err
+    integrator = np.maximum(np.minimum(integrator, integrator_clip), -integrator_clip)
+    integral = ki*integrator
 
-    proportional = kp*theta*np.dot(J, w_error)
-    derivative = -kd*np.dot(J, w_dt)
-
-    u_dt = proportional + derivative
-    CLIP = 1.2
-    u_dt = np.maximum(np.minimum(u_dt, CLIP), -CLIP)
+    u_dt = proportional + derivative + integral
+    u_dt = np.maximum(np.minimum(u_dt, torque_clip), -torque_clip)
     us.append(u_dt)
 
-    # delta = np.dot(reduced_torque_inv, u_dt[0:2])
-    # yaw = 0.2 if (proportional[2] > 0) else -0.2
-
-    # d_dt = np.dot(D_inv, np.array([delta[0], delta[1], yaw, 0.6]))
-
+    TODO
     d_dt = np.dot(Dp_inv, np.array(list(u_dt)+[0.8]))
+
     for k in range(4):
       if d_dt[k] > 1.0:
         d_dt[k] = 1.0
@@ -108,7 +119,7 @@ def simulate():
   ax[0].plot(np.asarray(range(n+1))*dt_s, ds)
   ax[0].legend(["d1", "d2", "d3", "d4"])
   ax[1].plot(np.asarray(range(n+1))*dt_s, us)
-  ax[1].legend(["x", "y", "z"])
+  ax[1].legend(["y"])
   plt.show()
 
 if __name__ == "__main__":
